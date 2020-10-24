dfd7e25686095a5ea2c8eabec692f214a62ea56a
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-24 19:08:01 +0530

    [Apoorva] Add . Welcome to Snake and Ladder Game

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    class Program
+    {
+        /// <summary>
+        /// Snake and Ladder Game Program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+        }
+    }
+}

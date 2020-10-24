15b597fa2bf3263a7fe19ce07d99eedc3936577b
Apoorva1217 <apoorvarasal12@gmail.com>
2020-10-24 19:24:34 +0530

    [Apoorva] Add . Player rolls a die

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,11 +11,16 @@ namespace Snake_and_Ladder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Game!");
-	    //Constants
+            //Constants
             const int START_POSITION = 0;
             const int FINAL_POSITION = 100;
 	    //Variable
             int positionOfPlayer = START_POSITION;
+			
+	    //The player rolls a die to get a number between 1 to 6
+            Random random = new Random();
+            int diceNumber = random.Next(0, 7);
+            Console.WriteLine("Number occured on Dice is:" + diceNumber);
         }
     }
 }
